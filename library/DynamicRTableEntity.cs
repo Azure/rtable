dfe14deb1f449334d8a607b2dc43ecbfecff3efa
ashwinmurthy <ashwinmurthy@gmail.com>
2015-01-29 18:21:31 -0800

    Added license headers and removed unused using statemetns

--- library/DynamicRTableEntity.cs
+++ library/DynamicRTableEntity.cs
@@ -1,16 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.WindowsAzure.Storage;
-using Microsoft.WindowsAzure.Storage.Core.Util;
-using Microsoft.WindowsAzure.Storage.Auth;
-using Microsoft.WindowsAzure.Storage.Table;
-using System.Configuration;
+﻿//-----------------------------------------------------------------------
+// <copyright file="CommandLineArgument.cs" company="Microsoft">
+//    Copyright 2013 Microsoft Corporation
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+// </copyright>
+//-----------
 
 namespace Microsoft.WindowsAzure.Storage.RTable
 {
+    using System;
+    using System.Collections.Generic;
+    using Microsoft.WindowsAzure.Storage;
+    using Microsoft.WindowsAzure.Storage.Table;
+
     /// <summary>
     /// DynamicRTableEntity2 is the same as DynamicRTableEntity except KeyNotFoundException is caught in ReadEntity().
     /// DynamicRTableEntity2 is used instead of DynamicRTableEntity when ConvertXStoreTableMode = true.
