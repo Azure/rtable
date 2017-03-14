622cb17ee7b966e8508ab6d36535aec2c23675bf
kasbaia <kasbaia@microsoft.com>
2017-03-13 17:06:07 -0700

    Adding perf. UTs for XSTore API to get reference numbers

---
+++ Test/Tests/RTableCRUDPerfTests.cs
@@ -0,0 +1,337 @@
+﻿// azure-rtable ver. 0.9
+//
+// Copyright (c) Microsoft Corporation
+//
+// All rights reserved.
+//
+// MIT License
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files
+// (the ""Software""), to deal in the Software without restriction, including without limitation the rights to use, copy, modify,
+// merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished
+// to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+// FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
+namespace Microsoft.Azure.Toolkit.Replication.Test
+{
+    using NUnit.Framework;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net;
+    using System.Text;
+    using Microsoft.WindowsAzure.Storage.Table;
+
+    /// <summary>
+    /// Keeps track of data points, and returns min/max/avg
+    /// </summary>
+    internal class DataSampling
+    {
+        private readonly string sampleName;
+        private readonly List<long> samples = new List<long>();
+        private long sum = 0;
+
+        public DataSampling(string sampleName)
+        {
+            if (string.IsNullOrEmpty(sampleName))
+            {
+                throw new ArgumentException("sampleName is null or empty!");
+            }
+
+            this.sampleName = sampleName;
+
+            Min = Int64.MaxValue;
+            Max = -1;
+            sum = 0;
+        }
+
+        public long Min { get; private set; }
+        public long Max { get; private set; }
+
+        public long Avg
+        {
+            get
+            {
+                if (samples.Count == 0)
+                {
+                    throw new ArgumentException("no data sampling!");
+                }
+
+                return sum / samples.Count;
+            }
+        }
+
+        public new string ToString()
+        {
+            return samples.Count == 0
+                        ? "there are no samples!!!!"
+                        : string.Format("{0}:\tmin={1}, max={2}, avg={3}", sampleName, Min, Max, Avg);
+        }
+
+        /// <summary>
+        /// Add a sample to the data
+        /// </summary>
+        /// <param name="curr"></param>
+        public void AddPoint(long curr)
+        {
+            Min = Math.Min(curr, Min);
+            Max = Math.Max(curr, Max);
+            sum += curr;
+
+            samples.Add(curr);
+        }
+
+        /// <summary>
+        /// Dump samples to a file
+        /// </summary>
+        /// <param name="path"></param>
+        public void DumpToFile(string path)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+    [TestFixture]
+    public class RTableCRUDPerfTests : RTableWrapperTestBase
+    {
+        // Sampling size = NumberOfPartitions * NumberOfRowsPerPartition
+        private const int NumberOfPartitions = 10;
+        private const int NumberOfRowsPerPartition = 5;
+
+        #region XStore Test
+
+        /// <summary>
+        /// Test XStore API to get perf. reference numbers
+        /// </summary>
+        [Test(Description = "XStore CRUD perf. numbers")]
+        public void XStoreCRUDTest()
+        {
+            try
+            {
+                this.LoadTestConfiguration();
+                string tableName = this.GenerateRandomTableName("XStorePerf");
+                Console.WriteLine("tableName = {0}", tableName);
+                this.SetupRTableEnv(tableName, true, "", new List<int> { 0 });
+
+                CloudTableClient tableClient = this.cloudTableClients[0];
+                CloudTable table = tableClient.GetTableReference(tableName);
+
+                // Generate entries, and Shuffle them ...
+                List<CustomerEntity> entries = GenerateEntries();
+                if (entries == null || !entries.Any())
+                {
+                    throw new ArgumentException("list is empty!");
+                }
+
+                Console.WriteLine("[C]reate stats ...");
+                entries = ShuffleList(entries);
+                DataSampling createStats = CreateEntriesStats(table, entries);
+
+                Console.WriteLine("[R]etrieve stats ...");
+                entries = ShuffleList(entries);
+                DataSampling retrieveStats = RetrieveEntriesPerf(table, entries);
+
+                Console.WriteLine("[U]pdate stats ...");
+                entries = ShuffleList(entries);
+                DataSampling updateStats = UpdateEntriesPerf(table, entries);
+
+                Console.WriteLine("[D]elete stats ...");
+                entries = ShuffleList(entries);
+                DataSampling deleteStats = DeleteEntriesPerf(table, entries);
+
+
+                // TODO: Add perf. tests for LINQ query ...
+
+
+                IEnumerable<CustomerEntity> customers = GetCustomerEntities(table);
+                Assert.AreEqual(customers.Count(), 0);
+
+                var report = new StringBuilder();
+                report.AppendLine("XStore CRUD perf (ms)");
+                report.AppendFormat("{0}\n", createStats.ToString());
+                report.AppendFormat("{0}\n", retrieveStats.ToString());
+                report.AppendFormat("{0}\n", updateStats.ToString());
+                report.AppendFormat("{0}\n", deleteStats.ToString());
+                report.AppendLine("End report.");
+
+                Console.WriteLine(report);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("{0}", ex);
+                Assert.Fail();
+            }
+            finally
+            {
+                base.DeleteAllRtableResources();
+            }
+        }
+
+        private DataSampling CreateEntriesStats(CloudTable table, List<CustomerEntity> entries)
+        {
+            var stats = new DataSampling("[C]reate");
+
+            foreach (var entry in entries)
+            {
+                TableOperation insertOperation = TableOperation.Insert(entry);
+
+                TableResult insertResult = null;
+                var watch = System.Diagnostics.Stopwatch.StartNew();
+                {
+                    insertResult = table.Execute(insertOperation);
+                }
+                watch.Stop();
+                stats.AddPoint(watch.ElapsedMilliseconds);
+
+                Assert.IsNotNull(insertResult, "insertResult = null");
+                Assert.AreEqual((int)HttpStatusCode.NoContent, insertResult.HttpStatusCode, "insertResult.HttpStatusCode mismatch");
+            }
+
+            return stats;
+        }
+
+        private DataSampling RetrieveEntriesPerf(CloudTable table, List<CustomerEntity> entries)
+        {
+            var stats = new DataSampling("[R]etrieve");
+
+            foreach (var entry in entries)
+            {
+                TableOperation retrieveOperation = TableOperation.Retrieve<CustomerEntity>(entry.PartitionKey, entry.RowKey);
+
+                TableResult retrieveResult = null;
+                var watch = System.Diagnostics.Stopwatch.StartNew();
+                {
+                    retrieveResult = table.Execute(retrieveOperation);
+                }
+                watch.Stop();
+                stats.AddPoint(watch.ElapsedMilliseconds);
+
+                Assert.IsNotNull(retrieveResult, "retrieveResult = null");
+                Assert.AreEqual((int)HttpStatusCode.OK, retrieveResult.HttpStatusCode, "retrieveResult.HttpStatusCode mismatch");
+
+                var customer = (CustomerEntity)retrieveResult.Result;
+                Assert.IsNotNull(customer, "Retrieve: customer = null");
+            }
+
+            return stats;
+        }
+
+        private DataSampling UpdateEntriesPerf(CloudTable table, List<CustomerEntity> entries)
+        {
+            var stats = new DataSampling("[U]pdate");
+
+            foreach (var entry in entries)
+            {
+                TableOperation retrieveOperation = TableOperation.Retrieve<CustomerEntity>(entry.PartitionKey, entry.RowKey);
+                TableResult retrieveResult = table.Execute(retrieveOperation);
+
+                Assert.IsNotNull(retrieveResult, "retrieveResult = null");
+                Assert.AreEqual((int)HttpStatusCode.OK, retrieveResult.HttpStatusCode, "retrieveResult.HttpStatusCode mismatch");
+                Assert.IsNotNull((CustomerEntity)retrieveResult.Result, "Retrieve: customer = null");
+
+                // Update entity
+                var customer = (CustomerEntity)retrieveResult.Result;
+                customer.Email = string.Format("{0}.{1}@email.com", entry.PartitionKey, entry.RowKey);
+
+                TableOperation updateOperation = TableOperation.Replace(customer);
+
+                TableResult updateResult;
+                var watch = System.Diagnostics.Stopwatch.StartNew();
+                {
+                    updateResult = table.Execute(updateOperation);
+                }
+                watch.Stop();
+                stats.AddPoint(watch.ElapsedMilliseconds);
+
+                Assert.IsNotNull(updateResult, "updateResult = null");
+                Assert.AreEqual((int)HttpStatusCode.NoContent, updateResult.HttpStatusCode, "updateResult.HttpStatusCode mismatch");
+            }
+
+            return stats;
+        }
+
+        private DataSampling DeleteEntriesPerf(CloudTable table, List<CustomerEntity> entries)
+        {
+            var stats = new DataSampling("[D]elete");
+
+            foreach (var entry in entries)
+            {
+                TableOperation retrieveOperation = TableOperation.Retrieve<CustomerEntity>(entry.PartitionKey, entry.RowKey);
+                TableResult retrieveResult = table.Execute(retrieveOperation);
+
+                Assert.IsNotNull(retrieveResult, "retrieveResult = null");
+                Assert.AreEqual((int)HttpStatusCode.OK, retrieveResult.HttpStatusCode, "retrieveResult.HttpStatusCode mismatch");
+                Assert.IsNotNull((CustomerEntity)retrieveResult.Result, "Retrieve: customer = null");
+
+                // Delete entity
+                var customer = (CustomerEntity)retrieveResult.Result;
+
+                TableOperation deleteOperation = TableOperation.Delete(customer);
+
+                TableResult deleteResult;
+                var watch = System.Diagnostics.Stopwatch.StartNew();
+                {
+                    deleteResult = table.Execute(deleteOperation);
+                }
+                watch.Stop();
+                stats.AddPoint(watch.ElapsedMilliseconds);
+
+                Assert.IsNotNull(deleteResult, "deleteResult = null");
+                Assert.AreEqual((int)HttpStatusCode.NoContent, deleteResult.HttpStatusCode, "deleteResult.HttpStatusCode mismatch");
+            }
+
+            return stats;
+        }
+
+        private IEnumerable<CustomerEntity> GetCustomerEntities(CloudTable table)
+        {
+            TableQuery<CustomerEntity> query = table.CreateQuery<CustomerEntity>();
+            return query.Execute();
+        }
+
+        #endregion
+
+
+        #region Helper Methods
+
+        private static List<CustomerEntity> GenerateEntries()
+        {
+            var entries = new List<CustomerEntity>();
+
+            for (int partitionId = 0; partitionId < NumberOfPartitions; partitionId++)
+            {
+                for (int rowId = 0; rowId < NumberOfRowsPerPartition; rowId++)
+                {
+                    string partitionKey = partitionId.ToString();
+                    string rowKey = rowId.ToString();
+
+                    entries.Add(new CustomerEntity(partitionKey, rowKey));
+                }
+            }
+
+            return entries;
+        }
+
+        private static List<CustomerEntity> ShuffleList(List<CustomerEntity> list)
+        {
+            if (list == null || !list.Any())
+            {
+                return list;
+            }
+
+            var rnd = new Random();
+            var size = list.Count;
+            return list.OrderBy(e => rnd.Next(size)).ToList();
+        }
+
+        #endregion
+    }
+}
