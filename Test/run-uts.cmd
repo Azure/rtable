0ebe3cb0d45332bbccd1373b7b6e21cc8e4cff63
Parveen-Patel-msft <parveenp@microsoft.com>
2017-04-25 11:02:16 -0700

    Update NUnit package
    change run-UTs command to run with new package.

--- Test/run-uts.cmd
+++ Test/run-uts.cmd
@@ -1,7 +1,9 @@
 for /f %%i in (%~dp0\parallel-tests.txt) do (
   del %%i.results.xml
+  start "%%i" ..\..\packages\NUnit.ConsoleRunner.3.6.1\tools\nunit3-console.exe Microsoft.Azure.Toolkit.Replication.Test.dll --test=%%i --out=%%i.results.xml
 )
 
 for /f %%i in (%~dp0\serial-tests.txt) do (
   del %%i.results.xml
+  start "%%i" /wait ..\..\packages\NUnit.ConsoleRunner.3.6.1\tools\nunit3-console.exe Microsoft.Azure.Toolkit.Replication.Test.dll --test=%%i --out=%%i.results.xml
 )
