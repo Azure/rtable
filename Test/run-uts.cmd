119ad58a8adcd6af277d342175a469b63b2d5ab5
Parveen-Patel-msft <parveenp@microsoft.com>
2017-04-25 10:59:50 -0700

    Update NUnit package
    - update nunit package
    - include NUnit.Runners package so it is not a separate download

--- Test/run-uts.cmd
+++ Test/run-uts.cmd
@@ -1,9 +1,7 @@
 for /f %%i in (%~dp0\parallel-tests.txt) do (
   del %%i.results.xml
-  start "%%i" ..\..\packages\NUnit.Runners.2.6.4\tools\nunit-console.exe Microsoft.Azure.Toolkit.Replication.Test.dll /run=%%i /xml=%%i.results.xml
 )
 
 for /f %%i in (%~dp0\serial-tests.txt) do (
   del %%i.results.xml
-  start "%%i" /wait ..\..\packages\NUnit.Runners.2.6.4\tools\nunit-console.exe Microsoft.Azure.Toolkit.Replication.Test.dll /run=%%i /xml=%%i.results.xml
 )
